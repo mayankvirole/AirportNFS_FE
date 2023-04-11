16972cf98a84220b8814e4901fa879a5fc2d90e7
mayank virole <mayank@nextsolutions.in>
2023-04-11 13:46:48 +0530

    fixes

--- src/components/Arrival.jsx
+++ src/components/Arrival.jsx
@@ -4,14 +4,13 @@ import {useEffect,useState} from 'react';
 import Nav from './Nav';
 import axios from  "axios";
 import Loader from './Loader';
-
+import swal from "sweetalert";
 const Arrival=() => {
 
 	const [airline, setAirline] = useState('');
 	const [destination, setDestination]= useState('');
 	let [data, setData] = useState([]);
-	const [arrayAirlines, setA1] =useState([]);
-	const [arrayDestination, setA2] =useState([]);
+
 	const [loading, setLoading] = useState(false);
 
 	const handleChangeA = (e) => {
@@ -23,37 +22,15 @@ const Arrival=() => {
 	}
 
 	const onClick = () => {
-		axios.post("https://major-be.onrender.com/get-by-airport", { destination, airlines : airline})
+		axios.post("https://major-be.onrender.com/get-flight-by-airport", { destination, airlines : airline})
 		.then((res) => { 
 			console.log(res.data)
-		})
-	}
-
-	const getData = () => {
-		setLoading(true);
-		axios.get("https://major-be.onrender.com/flight-details").then((res) => {
 			setData(res.data);
-			setLoading(false);
+			if(res.data.length <=1 )
+			swal("Error","Data not found", "error")
 		})
 	}
 
-	const setArray = () => {
-		let array = data.map((d) =>d.airlines)
-		setA1(()=>{
-			let a1 = array;
-			setA1(a1);
-		});
-		array = data.map((d) => d.destination)
-		setA2(array); 
-	}
-	console.log(arrayAirlines);
-	useEffect(() => {
-		getData()
-	},[]);
-
-	useEffect(() => {
-		setArray()
-	},[]);
 
 	return (
 		<div className='arr'>
@@ -75,42 +52,38 @@ const Arrival=() => {
 				</nav>
 			<h2>Flight Arrival Timings</h2>
 
-			{!loading ? 
 			<span className='inner'>
-				<label for="air">Airport</label>
-				<select id='air' onChange={handleChangeA} value={destination}>
-					<option value="" >Select an airport</option>
-					{arrayAirlines.map((d) => { 
-					return <option value={d.destination}>{d.destination}</option>})}
-				</select>
+				<label for="air">Airlines</label>
+				<input value={airline} onChange={handleChangeA}></input>
 
-				<label for = "">Terminal</label>
-				<select onChange={handleChangeD} value={airline} >
-					<option value="" >Select an airline</option>
-					{arrayDestination.map((d) => { 
-					return <option value={d.airlines}>{d.airlines}</option>})}
-				</select>
-			</span> : <Loader />}
+				<label for = "">Destination</label>
+				<input value={destination} onChange={handleChangeD}></input>
+			</span>
 
 			<button type='submit' onClick={onClick} >Search</button>
 			
 			<>
-				{ arrayAirlines && arrayAirlines.length >0 && 
+				{ data && data.length >0 && 
 				<table>
 					<tr>
 						<th>Flight Number</th>
 						<th>Destination</th>
 						<th>Arrival</th>
 						<th>Airline</th>
-						<th>Duration</th>
+						<th>Source</th>
 					</tr>
-					<tr>
-						<td>1099</td>
-						<td>Raja Bhoj Airport</td>
-						<td>10:45</td>
-						<td>2A</td>
-						<td>02:00</td>
-					</tr>
+
+					{(data&&data.map((d) => (
+						<>
+							<tr>
+								<td>{d.flight_number}</td>
+								<td>{d.destination}</td>
+								<td>{d.arrival}</td>
+								<td>{d.airlines}</td>
+								<td>{d.source}</td>
+							</tr>
+						</>
+					)))}
 				</table>
 				}		
 			</>
--- src/components/Departure.jsx
+++ src/components/Departure.jsx
@@ -2,81 +2,90 @@ import {Link} from 'react-router-dom';
 import "../css/Arrival.css";
 import {useEffect,useState} from 'react';
 import Nav from './Nav';
+import axios from  "axios";
+import Loader from './Loader';
+import swal from "sweetalert";
 
 const Departure=() => {
 
-	const [airport, setAirport] = useState('');
-	const [airports,setAirports]=useState([]);
-	const [terminals,setTerminals]=useState([]);
-	const [data, setData] = useState([]);
-	const [terminal, setTerminal] = useState('');
-	
-	const onClick = () => {
+	const [airline, setAirline] = useState('');
+	const [source, setSource]= useState('');
+	const [data,setData]=useState([]);
+	const [terminal,setTerminal]=useState('');
 
+	const onClick=() => {
+		axios.post("https://major-be.onrender.com/get-flight-by-airport",{source,airlines: airline})
+			.then((res) => {
+				console.log(res.data)
+				setData(res.data);
+				if(res.data.length<=1)
+					swal("Error","Data not found","error")
+			})
 	}
 
-	const getData = () => {
-
+	const handleChangeA = (e) => {
+		setAirline(e.target.value);
 	}
-	useEffect(() => {
 
-	},[]);
+	const handleChangeD = (e) => {
+		setSource(e.target.value);
+	}
 
 	return (
 		<div className='arr'>
 			<nav>
-					<Nav />
-					<nav>
-						<ul className='nav'>
-							<li>
-								<Link to="/home">Home</Link>
-							</li>
-							<li>
-								<Link to="/arrival">Arrival</Link>
-							</li>
-							<li>
-								<Link to="/departure">Departure</Link>
-							</li>
-						</ul>
-					</nav>
+				<Nav />
+				<nav>
+					<ul className='nav'>
+						<li>
+							<Link to="/home">Home</Link>
+						</li>
+						<li>
+							<Link to="/arrival">Arrival</Link>
+						</li>
+						<li>
+							<Link to="/departure">Departure</Link>
+						</li>
+					</ul>
 				</nav>
+			</nav>
 			<h2>Flight Departure Timings</h2>
 
 			<span className='inner'>
-				<label for="air">Airport</label>
-				<select id='air'>
-					<option selected value={null}>Select Airport</option>
-					{airports.map((air) => {<option value={air.value}>{air.name}</option>})}
-				</select>
-			</span>
-			<span className='inner'>
-				<label for="ter">Terminal</label>
-				<select id='ter'>
-					<option selected value={null}>Select Terminal</option>
-					{terminals.map((ter) => {<option value={ter.value}>{ter.name}</option>})}
-				</select>
+				<label for="air">Airlines</label>
+				<input value={airline} onChange={handleChangeA}></input>
+
+				<label for="">Source</label>
+				<input value={source} onChange={handleChangeD}></input>
 			</span>
 
-			{/* <button type='submit' onClick={onClick} >Search</button> */}
-			{ 
-			<>
-				<table>
-					<tr>
-						<th>Flight Number</th>
-						<th>Destination</th>
-						<th>Departure</th>
-						<th>Airline</th>
-						<th>Duration</th>
-					</tr>
-					<tr>
-						<td>1099</td>
-						<td>Raja Bhoj Airport</td>
-						<td>10:45</td>
-						<td>2A</td>
-						<td>02:00</td>
-					</tr>
-				</table>
-			</>}
+			<button type='submit' onClick={onClick} >Search</button>
+			{
+				<>
+					{data&&data.length>0&&
+						<table>
+							<tr>
+								<th>Flight Number</th>
+								<th>Destination</th>
+								<th>Departure</th>
+								<th>Airline</th>
+								<th>Source</th>
+							</tr>
+
+							{(data&&data.map((d) => (
+								<>
+									<tr>
+										<td>{d.flight_number}</td>
+										<td>{d.destination}</td>
+										<td>{d.departure}</td>
+										<td>{d.airlines}</td>
+										<td>{d.source}</td>
+									</tr>
+								</>
+							)))}
+						</table>
+					}
+				</>}
 		</div>
 	)
 }
